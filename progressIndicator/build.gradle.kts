8d1bf1167fa4ac215a24fda42708541bdd30502c
邹铭杰 <zoumingjie17@163.com>
2023-09-19 16:03:07 +0800

    feat:Remove Unused Resources

--- progressIndicator/build.gradle.kts
+++ progressIndicator/build.gradle.kts
@@ -32,8 +32,6 @@ kotlin {
                 implementation(compose.runtime)
                 implementation(compose.foundation)
                 implementation(compose.material)
-                @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
-                implementation(compose.components.resources)
             }
         }
         val androidMain by getting {
--- progressIndicator/src/commonMain/kotlin/com/multiplatform/progressindicator/sample/BasicProgressIndicatorSample.kt
+++ progressIndicator/src/commonMain/kotlin/com/multiplatform/progressindicator/sample/BasicProgressIndicatorSample.kt
@@ -26,7 +26,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import progressindicator.SimpleProgressIndicatorWithAnim
+import com.multiplatform.progressindicator.SimpleProgressIndicatorWithAnim
 
 /**
  * Created By Kevin Zou On 2023/9/13
